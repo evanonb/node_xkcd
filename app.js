f79c908357a1a925c42626bf96f3a16d841dcaba
evanonb <owenby_evan@columbusstate.edu>
2022-10-19 23:10:53 +0000

    it works!

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var axios = require('axios');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    randomizer = Math.floor(Math.random() * 100) + 1;
+    axios.get('https://xkcd.com/'+randomizer+'/info.0.json').then(function(response){
+        res.render('index.ejs', {comicData: response.data});
+    })
+})
+
+app.listen(3000, function(){
+    console.log('App listening on port 3000');
+})
